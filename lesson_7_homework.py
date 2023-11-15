f2eb32b232ad3dd475769fa53298ccc12134b5a7
imKochurov <i.m.kochurov@gmail.com>
2023-11-15 10:45:44 +0200

    initial commit

--- lesson_7_homework.py
+++ lesson_7_homework.py
@@ -1,44 +1,44 @@
 # TASK 1
 
-# dict_final = dict()
+dict_final = dict()
 
-# while True:
-#     a = input('Введіть слово. Для виходу з програми введіть "q":\n')
-#     if a == 'q':
-#         break
-#     elif dict_final.get(a) == None:
-#         dict_final.update({a: 0})
-#     else:
-#         dict_final[a] += 1
-#     print(dict_final)
+while True:
+    a = input('Введіть слово. Для виходу з програми введіть "q":\n')
+    if a == 'q':
+        break
+    elif dict_final.get(a) == None:
+        dict_final.update({a: 0})
+    else:
+        dict_final[a] += 1
+    print(dict_final)
 
 
 # TASK 2
 
-# stock = {
-#     "banana": 6,
-#     "apple": 0,
-#     "orange": 32,
-#     "pear": 15
-# }
-# prices = {
-#     "banana": 4,
-#     "apple": 2,
-#     "orange": 1.5,
-#     "pear": 3
-# }
+stock = {
+    "banana": 6,
+    "apple": 0,
+    "orange": 32,
+    "pear": 15
+}
+prices = {
+    "banana": 4,
+    "apple": 2,
+    "orange": 1.5,
+    "pear": 3
+}
 
-# total_price = dict()
+total_price = dict()
 
-# for a in stock:
-#     total_price.update({a: stock[a]*prices[a]})
+for a in stock:
+    total_price.update({a: stock[a]*prices[a]})
 
-# print (total_price)
+print (total_price)
 
 # TASK 3
 
-# a = [(x, x**2) for x in range(1, 11)]
-# print(a)
+a = [(x, x**2) for x in range(1, 11)]
+print(a)
 
 # TASK 4
 
