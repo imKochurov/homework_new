5f8a2801a2f42009fb2f0172eca3856a1930eaf2
imKochurov <i.m.kochurov@gmail.com>
2024-02-22 20:09:20 +0200

    first commit

---
+++ django_homework/my_project/config.py
@@ -0,0 +1,7 @@
+SECRET_KEY = 'django-insecure-+78-$dx-m+4jg@9*j5jjs4$(86&ms97%rn$o5#9d3t+c1)$i@d'
+
+NAME_DB = 'my_studying_schema'
+USER_DB = 'user_test'
+PASSWORD_DB = 'SQL_pass*2112'
+HOST_DB = '127.0.0.1'
+PORT_DB = '3306'
